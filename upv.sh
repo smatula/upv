7038423ec54f7739ffc460635d412e3087e1c9cc
Stephen Matula <smatula@redhat.com>
2023-11-29 14:21:41 -0500

    New options, flow, checks and cleanup

--- upv.sh
+++ upv.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 set -o pipefail
 
+# Function to display usage/help
+usage () {
+    echo "Usage: upv.sh [options] -o ver -n ver [-f file | jobnames]
+
+Update lp interop scenarios in openshift-ci to run on new platform version.
+
+NOTE: Must be executed from openshift-ci's top level directory 'release'.
+
+Example: upv.sh -o 4.15 -n 4.16 periodic-ci-skupperproject-skupper-openshift-smoke-test-image-main-service-interconnect-ocp4.15-lp-interop-rhsi-interop-aws
+
+Options:
+    Required:
+        -o|--old_ver ver     - Old platform version.
+        -n|--new_ver ver     - New platform version.
+    Optional:
+        -p|--platform str    - Platform, str is ocp or hypershift. Default is ocp.
+        -t|--lp_tag str      - Overide default tag, -lp-interop for ocp and -lp-rosa-hypershift for hypershift
+                               str is new tag. Searches for tag in config file name or its contents.
+        -i|--text            - Flag input file format is text. Single job name per line.
+        -z|--z_stream        - [Not implemented] Flag indicating to update old version config file to be z-stream, stream: stable
+        -f|--input_file file - Input file containing list of jobs to update. JSON format.
+                               See vault trigger file.
+    jobnames                 - Scenario's job name/s seperated by space.
+    "
+}
+
+# Initialize
 DAY=$( date +%-d )
 MONTH=$( date +%-m )
 JOBS_ARRAY=()
+JOBS=""
 
 # Default PLATFORM
 PLATFORM="ocp"
@@ -11,12 +39,24 @@ PLATFORM="ocp"
 # Default LP_TAG
 LP_TAG="\-lp\-interop"
 
+# default Z_STREAM
+Z_STREAM="false"
+
+# Default FILE_FMT
+FILE_FMT="json"
+
 POSITIONAL_ARGS=()
 
+# Process command line
 while [[ $# -gt 0 ]]; do
   case $1 in
     -p|--platform)
       PLATFORM="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -t|--lp_tag)
+      LP_TAG="$2"
       shift # past argument
       shift # past value
       ;;
@@ -35,33 +75,26 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
+    -i|--text)
+      FILE_FMT="txt"
+      shift # past argument
+      ;;
+    -z|--z_stream)
+      Z_STREAM="false"
+      shift # past argument
+      ;;
     -h|--help)
-      echo "Usage: upv.sh [options] -o ver -n ver [-f file | jobname]
-
-Update lp interop scenarios in openshift-ci to run on new platform version.
-
-NOTE: Must be executed from openshift-ci's top level directory 'release'.
-
-Example: upv.sh -o 4.15 -n 4.16 periodic-ci-skupperproject-skupper-openshift-smoke-test-image-main-service-interconnect-ocp4.15-lp-interop-rhsi-interop-aws
-
-Options:
-    Required:
-        -o|--old_ver ver     - Old platform version.
-        -n|--new_ver ver     - New platform version.
-    Optional:
-        -p|--platform str    - Platform, str is ocp or hypershift. Default is ocp.
-        -f|--input_file file - Input file containing list of jobs to update. JSON format.
-                               See vault trigger file.
-    jobname                  - Scenario's job name.
-      "
+      usage
       exit 0
       ;;
     -*|--*)
       echo "Unknown option $1"
+      usage
       exit 1
       ;;
     *)
       POSITIONAL_ARGS+=("$1") # save positional arg
+      JOBS=`echo $JOBS;printf %"s\n" ${1}`
       shift # past argument
       ;;
   esac
@@ -85,48 +118,83 @@ echo "PLATFORM     = ${PLATFORM}"
 echo "LP_TAG       = ${LP_TAG}"
 echo "PWD          = " `pwd`
 
-# Check that old and new platform versions supplied.
+# Check that OLD_VER and NEW_VER platform versions supplied.
 if [ -z "$OLD_VER" ] || [ -z "$NEW_VER" ]
 then
+    echo ""
     echo "ERROR: OLD and NEW Platform versions must be supplied"
+    echo ""
+    usage
     exit 1
 fi
 
-# If PLATFORM not ocp or hypershift then error
+# Check PLATFORM set to ocp or hypershift
 if [[ $PLATFORM != "ocp" && $PLATFORM != "hypershift" ]]
 then
+    echo ""
     echo "ERROR: PLATFORM must be set to either 'ocp' or 'hypershift'"
+    echo ""
+    usage
     exit 1
 fi
 
-# Get current directory and if not release error
+# Check current directory is correct - Should be in release directory
 DIR=${PWD/*\//}
 if [[ $DIR != "release" ]]
 then
+    echo ""
     echo "ERROR: Must run from openshift-ci's top level directory: release"
+    echo ""
+    usage
     exit 1
 fi
 
-# PROCESS JOBS
+# Set JOBS if input file supplied - Expected to be in format of vault trigger file
 if [ ! -z "$INPUT_FILE" ]
 then
+    # Check file exists
     if ! test -f $INPUT_FILE
     then
+        echo ""
         echo "ERROR: Input file ${INPUT_FILE} dost not exist"
+        echo ""
+        useage
         exit 1
     fi
-    JOBS=`jq -r '.[] | select(.active == true) | .job_name' "$INPUT_FILE"`
-else
-    JOBS=$1
-    if [ -z "$JOBS" ]
+
+    if [[ $FILE_FMT != "json" ]]
     then
-        echo "ERROR: No job name or input file given"
-        exit 1
+        # Get list of jobs from text file, remove blank lines
+        JOBS=`grep '[^[:blank:]]' < "$INPUT_FILE"`
+    else
+        # Get list of jobs from json file, Should be in format of vault trigger file
+
+        # Check Valid json
+        if JOBS=`jq -re '.[] | select(.active == true) | .job_name' "$INPUT_FILE"`
+        then
+            echo "Parsed JSON file successfully."
+        else
+            echo "ERROR: Failed to parse JSON file, or got false/null"
+            echo ""
+            usage
+            exit 1
+        fi
     fi
+fi
+
+# Check we have JOBS to process
+if [ -z "$JOBS" ]
+then
+    echo ""
+    echo "ERROR: No job name or input file given"
+    echo ""
+    usage
+    exit 1
 fi
 
 # For each ENTRY (JOB) perform the following
 while IFS= read -r ENTRY; do
+    E_OLD_VER=""
 
     # ENTRY single job name
     echo ""
@@ -141,18 +209,19 @@ while IFS= read -r ENTRY; do
     fi
     echo "JOB          = ${JOB}"
 
-    # JOB_PATH path of JOB
+    # Set JOB_PATH and CONFIG_PATH
+    # JOB_PATH path of JOB's file
     # CONFIG_PATH path of JOB's config file
     JOB_PATH="$(dirname "${JOB}")"
     CONFIG_PATH=${JOB_PATH/jobs/config}
 
     echo "JOB PATH     = ${JOB_PATH}"
 
-    # Set NEW_JOB using FMT1 (4.12) - ENTRY name with new version
+    # Set NEW_JOB name using FMT1 (4.12) - ENTRY name with new version
     NEW_JOB=${ENTRY//${OLD_VER}/${NEW_VER}}
     echo "NEW JOB      = ${NEW_JOB}"
 
-    # If same set NEW_JOB using FMT2 (4-12) 
+    # If same (Name not changed) set NEW_JOB using FMT2 (4-12)
     if test "$ENTRY" = "$NEW_JOB"
     then
         E_OLD_VER=${OLD_VER/./-}
@@ -163,7 +232,7 @@ while IFS= read -r ENTRY; do
         NEW_JOB=${ENTRY//${E_OLD_VER}/${E_NEW_VER}}
     fi
 
-    # If same set NEW_JOB using FMT3 (412)
+    # If same (Name not changed) set NEW_JOB using FMT3 (412)
     if test "$ENTRY" = "$NEW_JOB"
     then
         E_OLD_VER=${OLD_VER/./""}
@@ -259,12 +328,16 @@ while IFS= read -r ENTRY; do
     # Update version in config file
     sed -i "s/$SED_OLD_VER/$SED_NEW_VER/g" $CONFIG_PATH/$NEW_CONFIG_FILE
 
-    # If alternate format used also perform update with alternate fmt 
+    # If alternate format used also perform update with alternate fmt also
     if [[ ! -z "$E_OLD_VER" ]]
     then
-        sed -i "s/$E_OLD_VER/$E_NEW_VER/g" $CONFIG_PATH/$NEW_CONFIG_FILE
+        # Set version to be sed correct
+        SED_E_OLD_VER=${E_OLD_VER/./\\.}
+        SED_E_NEW_VER=${E_NEW_VER/./\\.}
+        sed -i "s/$SED_E_OLD_VER/$SED_E_NEW_VER/g" $CONFIG_PATH/$NEW_CONFIG_FILE
     fi
 
+    # Handle any special processing for scnearios here....
     # Scenarios With Special processing
     if [[ $CONFIG_PATH == *"service-binding-operator"* ]]; then
        NEW_TAG=--tags=~@disable-openshift-$NEW_VER+
@@ -290,6 +363,13 @@ while IFS= read -r ENTRY; do
 
         sed -i "s/$i/$NEW_CRON/g" $CONFIG_PATH/$NEW_CONFIG_FILE
     done < <(echo "$CRON")
+
+    # Update old version to be z-stream stream: stable
+    if [[ $Z_STREAM  == "true" ]]
+    then
+       sed -i "s/stream: nightly/stream: stable/g" $CONFIG_PATH/$CONFIG_FILE
+       git add $CONFIG_PATH/$CONFIG_FILE
+    fi
 
     # Git add new config file
     git add $CONFIG_PATH/$NEW_CONFIG_FILE
@@ -368,6 +448,19 @@ for (( i=0; i<${#JOBS_ARRAY[@]} ; i+=3 )) ; do
     if test -f $PRESUBMITS
     then
         git add $PRESUBMITS
+    fi
+
+    # Update old files to be z-stream if flag set
+    if [[ $Z_STREAM == "true" ]]
+    then
+        git add $OLD_JOB
+
+        OLD_PRESUBMITS=${OLD_JOB/periodics/presubmits}
+
+        if test -f $OLD_PRESUBMITS
+        then
+            git add $OLD_PRESUBMITS
+        fi
     fi
 done
 
